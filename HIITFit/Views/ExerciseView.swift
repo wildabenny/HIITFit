f6ac916a2c0687a9c6332be8787e177edf51aaba
Marcin Strzelczyk <brancodev@gmail.com>
2021-08-31 00:54:51 +0200

    prototype views

--- HIITFit/Views/ExerciseView.swift
+++ HIITFit/Views/ExerciseView.swift
@@ -1,19 +1,37 @@
 
 import SwiftUI
+import AVKit
 
 struct ExerciseView: View {
   let videoNames = ["squat", "step-up", "burpee", "sun-salute"]
   let exerciseNames = ["Squat", "Step Up", "Burpee", "Sun Salute"]
   let index: Int
+    let interval: TimeInterval = 30
 
   var body: some View {
-    VStack {
-        HeaderView(excerciseName: exerciseNames[index])
-        Text("Video Player")
-        Text("Timer")
-        Text("Start/Done button")
-        Text("Rating")
-        Text("History button")
+    GeometryReader { geometry in
+        VStack {
+            HeaderView(excerciseName: exerciseNames[index])
+                .padding(.bottom)
+            if let url = Bundle.main.url(
+                forResource: videoNames[index], withExtension: "mp4") {
+                VideoPlayer(player: AVPlayer(url: url))
+                    .frame(height: geometry.size.height * 0.45)
+            } else {
+                Text("Couldn’t find \(videoNames[index]).mp4")
+                  .foregroundColor(.red)
+            }
+            Text(Date().addingTimeInterval(interval), style: .timer)
+              .font(.system(size: 90))
+            Button("Start/Done") {}
+                .font(.title3)
+                .padding()
+            RatingView()
+                .padding()
+            Spacer()
+            Button("History") {}
+                .padding(.bottom)
+        }
     }
   }
 }
---
+++ HIITFit/Views/RatingView.swift
@@ -0,0 +1,21 @@
+
+import SwiftUI
+
+struct RatingView: View {
+    var body: some View {
+        HStack {
+            ForEach(0 ..< 5) { _ in
+                Image(systemName: "waveform.path.ecg")
+                    .foregroundColor(.gray)
+                    .font(.largeTitle)
+            }
+        }
+    }
+}
+
+struct RatingView_Previews: PreviewProvider {
+    static var previews: some View {
+        RatingView()
+            .previewLayout(.sizeThatFits)
+    }
+}
