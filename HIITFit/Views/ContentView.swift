98fb1611f0b5677313e4fac3f94aae6aae18d05f
Marcin Strzelczyk <brancodev@gmail.com>
2021-08-30 21:35:22 +0200

    Initial Commit

---
+++ HIITFit/Views/ContentView.swift
@@ -0,0 +1,20 @@
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    TabView {
+      WelcomeView()
+      ForEach(0 ..< 4) { index in
+        ExerciseView(index: index)
+      }
+    }
+    .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
---
+++ HIITFit/Views/ExerciseView.swift
@@ -0,0 +1,18 @@
+
+import SwiftUI
+
+struct ExerciseView: View {
+  let videoNames = ["squat", "step-up", "burpee", "sun-salute"]
+  let exerciseNames = ["Squat", "Step Up", "Burpee", "Sun Salute"]
+  let index: Int
+
+  var body: some View {
+    Text(exerciseNames[index])
+  }
+}
+
+struct ExerciseView_Previews: PreviewProvider {
+  static var previews: some View {
+    ExerciseView(index: 0)
+  }
+}
